237f9208b6b3fcb360efdb172e86ffaa7f2aa4ab
minghao.yin@accenture.com <minghao.yin@accenture.com>
2020-04-20 10:35:12 +0800

    1

--- index.js
+++ index.js
@@ -7,8 +7,11 @@ app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true }));
 var fs = require('fs');
 
+var request = require('request');
+
 app.use(express.static("public")).listen(8080,()=>{
     console.log('启动成功');
+    // dk();
 });
 
 app.all('*', function (req, res, next) {
@@ -17,40 +20,78 @@ app.all('*', function (req, res, next) {
     next();
 });
 
-app.post('/profile', upload.array(), function (req, res, next) {
-    var data = req.body.data;
+post('/profile', function(res){
+    var data = res.data;
     var d = data.split('\n');
     var jsonToday = [];
     d.forEach((lineOne)=>{
         jsonToday.push({entity: lineOne.split('\t')[0], city: lineOne.split('\t')[1], numberOfMasksReceived: lineOne.split('\t')[2]});
     });
 
-    var time = req.body.time;
+    var time = res.time;
     if (!time) {
         time = new Date().toLocaleDateString();
     }
+    time = getFirstDayOfWeek(time);
     fs.writeFileSync('./public/data/data'+ time + '.json', JSON.stringify({time: time, data: jsonToday, zs: req.body.zs}),'utf8');
-    res.send('OK');
-    next();
 });
 
-app.post('/toDayTj', upload.array(), function (req, res, next) {
-    var data = req.body.data;
-    var time = req.body.time;
+post('/toDayTj', function(res){
+    var data = res.data;
+    var time = res.time;
     if (!time) {
         time = new Date().toLocaleDateString();
     }
+    time = getFirstDayOfWeek(time);
     fs.writeFileSync('./public/data/data'+ time + '.json', JSON.stringify(data),'utf8');
-    res.send('OK');
-    next();
 });
 
-app.get('/all', (req, res)=>{
+get('/all', function(res){
     var data = fs.readFileSync('./public/data/data.json', {flag: 'r+', encoding: 'utf8'});
-    res.send(data);
+    return data;
 });
 
-app.get('/timeDate/:time', (req, res)=>{
-    var data = fs.readFileSync('./public/data/data'+ req.params.time +'.json', {flag: 'r+', encoding: 'utf8'});
-    res.send(data);
+get('/timeDate/:time', function(res){
+    var time = getFirstDayOfWeek(res.time);
+    var data = fs.readFileSync('./public/data/data'+ time +'.json', {flag: 'r+', encoding: 'utf8'});
+    return data;
 });
+
+
+function post(url, callback){
+    app.post(url, upload.array(), function (req, res, next) {
+        callback(req.body);
+        res.send('OK');
+        next();
+    });
+}
+
+function get(url, callback){
+    app.get(url, (req, res)=>{
+        var data = callback(req.params);
+        res.send(data);
+    });
+}
+
+function getFirstDayOfWeek (date) {
+    var day = new Date(date).getDay() || 7;
+    var one = new Date(date.getFullYear(), date.getMonth(), date.getDate() + 1 - day).getTime();
+    var fire = one + (1000 * 60 * 60 * 24 * 4);
+    return new Date(fire).toLocaleDateString();
+};
+
+// function dk(){
+//     now = new Date();
+//     request.get({
+//         // url:'https://view.inews.qq.com/g2/getOnsInfo?name=disease_h5&_t=0.9760834383815831&callback=__jpcb0',
+//         url: 'https://api.inews.qq.com/newsqa/v1/automation/foreign/country/ranklist',
+//         headers: {
+//             "Accept": "application/json;charset=UTF-8",
+//             "Cookie": "pac_uid=0_9cbc24b5f14b5; XWINDEXGREY=0; pgv_info=ssid=s8512551360; pgv_pvid=6405351180",
+//             "Content-Type": "application/json;charset=UTF-8",
+//             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36"
+//         }
+//     },function(error, response, body) {
+//         console.log(JSON.parse(body));
+//     })
+// }
